46089f521088d70d24f0bc678e39a2271e0f1d08
Jehad Abusir <jabusir@MacBook-Pro.local>
2020-06-21 00:44:56 -0700

    Hotfix

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { Route, Redirect, withRouter } from "react-router-dom";
 
 function PrivateRoute({ component: Component, auth, ...rest }) {
-    console.log(auth)
     return (
       <Route
         {...rest}
